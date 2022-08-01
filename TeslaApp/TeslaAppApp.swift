bb5e5964deff80d68e27163aca591a0edef1aba7
matheus3006 <matheussouza18@live.com>
2022-08-01 15:04:12 -0300

    Initial Commit

---
+++ TeslaApp/TeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppApp.swift
+//  TeslaApp
+//
+//  Created by Matheus Souza on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
