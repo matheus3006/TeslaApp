a2b7022d81b419b9cb9586bfa62332ed847d3532
matheus3006 <matheussouza18@live.com>
2022-08-01 16:12:44 -0300

    feat: create VoiceCommandButton() UI

--- TeslaApp/ContentView.swift
+++ TeslaApp/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            VoiceCommandButton()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct VoiceCommandButton: View {
+    var body: some View {
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(systemName:"mic.fill")
+                        .font(.system(size:24,weight:.semibold,design:
+                            .default))
+                        .frame(width: 64, height: 64)
+                        .background(Color.green)
+                        .clipShape(Circle())
+                        .padding()
+                        .shadow(radius: 10)
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
     }
 }
